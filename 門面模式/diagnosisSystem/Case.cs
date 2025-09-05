39d7e1954bbfc913fb18e18bc9b411836c0dce7a
PatrickChen <patrickchen@tw.maxxis.com>
2025-09-05 17:14:58 +0800

    #20250905 卡在維護者希望能夠在外部擴充新的處方診斷規則，而不用修改模組內部程式，是否新增一個規則和處方的字典集合?

--- 門面模式/diagnosisSystem/Case.cs
+++ 門面模式/diagnosisSystem/Case.cs
@@ -3,13 +3,13 @@
 public class Case
 {
     public List<Symptom> Symptoms { get; set; }
-    public Prescriber Prescriber { get; set; }
+    public Prescription Prescription { get; set; }
     public int CaseTime { get; set; }
 
-    public Case(List<Symptom> symptoms, Prescriber prescriber, int caseTime)
+    public Case(List<Symptom> symptoms, Prescription prescription, int caseTime)
     {
         Symptoms = symptoms;
-        Prescriber = prescriber;
+        Prescription = prescription;
         CaseTime = caseTime;
     }
 }
--- 門面模式/diagnosisSystem/IDiagnosisStrategy.cs
+++ 門面模式/diagnosisSystem/IDiagnosisStrategy.cs
@@ -2,32 +2,41 @@
 
 public interface IDiagnosisStrategy
 {
-    Prescription Diagnose(Patient patient, List<Symptom> symptoms);
+    Prescription CurrentPrescription { get; set; }
 }
 
-public class CovidStrategy : IDiagnosisStrategy
+public class SleepApneaStrategy : IDiagnosisStrategy
 {
-    public Prescription Diagnose(Patient patient, List<Symptom> symptoms)
+    public Prescription CurrentPrescription { get; set; }
+
+    public SleepApneaStrategy()
     {
-        var medicines = new List<Medicine>();
-        medicines.Add(new Medicine("清冠一號"));
-        return new Prescription("清冠一號", "新冠肺炎（專業學名：COVID-19）", medicines,
+        CurrentPrescription = new Prescription("清冠一號", "新冠肺炎（專業學名：COVID-19）",
+            new List<Medicine> { new Medicine("清冠一號") },
             "將相關藥材裝入茶包裡，使用500 mL 溫、熱水沖泡悶煮1~3 分鐘後即可飲用。");
     }
 }
 
 public class AttractiveStrategy : IDiagnosisStrategy
 {
-    public Prescription Diagnose(Patient patient, List<Symptom> symptoms)
+    public Prescription CurrentPrescription { get; set; }
+
+    public AttractiveStrategy()
     {
-        throw new NotImplementedException();
+        CurrentPrescription = new Prescription("青春抑制劑", "有人想你了 (專業學名：Attractive)",
+            new List<Medicine> { new Medicine("假鬢角"), new Medicine("臭味") },
+            "將相關藥材裝入茶包裡，使用500 mL 溫、熱水沖泡悶煮1~3 分鐘後即可飲用。");
     }
 }
 
-public class SleepApneaStrategy : IDiagnosisStrategy
+public class CovidStrategy : IDiagnosisStrategy
 {
-    public Prescription Diagnose(Patient patient, List<Symptom> symptoms)
+    public Prescription CurrentPrescription { get; set; }
+
+    public CovidStrategy()
     {
-        throw new NotImplementedException();
+        CurrentPrescription = new Prescription("打呼抑制劑", "睡眠呼吸中止症（專業學名：SleepApneaSyndrome）",
+            new List<Medicine> { new Medicine("一捲膠帶") },
+            "睡覺時，撕下兩塊膠帶，將兩塊膠帶交錯黏在關閉的嘴巴上，就不會打呼了。");
     }
 }
